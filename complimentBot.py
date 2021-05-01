dbf61dd5d1c7d42023c4c12864719b89710e9d09
alealclag <alealclag@alum.us.es>
2021-05-02 01:22:48 +0200

    Añadida la opción de imagen

--- complimentBot.py
+++ complimentBot.py
@@ -4,6 +4,7 @@ import random
 
 from tweepy.error import TweepError
 from botCredentials import *
+from cumplidos import *
 
 auth = tweepy.OAuthHandler(API_KEY,  API_SECRET_KEY)
 
@@ -11,12 +12,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
 
 api = tweepy.API(auth)
 lastMentionsID2 = []
-cumplidos = ["Guaperas", "Hermosura", "❤❤❤❤❤", "😍😍😍😍😍😍😍😍😍", "🥵🥵🥵🥵🥵🥵🥵🥵🥵", "😳😳😳😳😳😳😳", "Bellesón", "No te pongas al sol, quel los bombones se derriten",
-             "Vaya, no sabía que había gente tan hermosa en Twitter", "Por ti, mi corazón palpita, como una patata frita", "La verdad que te quedan muy bien los sombreros", "Damn u really hot",
-             "🏆 Premio a la persona más guuapa de Twitter", "Eres como una rana: muy bonita", "Yo me haría pirata. No por el oro ni por la plata. Sino por ese tesoro, que tienes entre las patas",
-             "Vaya peinado más guay tienes", "Hola, somos de la Agencia Tributaria de Belleza. Venimos a cobrarle el impuesto de las grandes fortunas",
-             "Si fuese camionero, te llevaría en mi camión. Pero como no lo soy, te llevo en mi corazón",  "Que tengas un buen día :)",
-             "¡Buenas! Soy el bot medidor de las personas guapas. Según mis cálculos ¿usted tiene una belleza del 100%!"]
+
 
 # credit for this function to Thomas Ashish Cherian (https://github.com/PandaWhoCodes)
 
@@ -56,13 +52,36 @@ def getRandomFriend(user):
         randomFriend]._json["screen_name"]
 
 
-for mention in api.mentions_timeline()[:5]:
+def postCompliment(user):
+    complimentPerc = 100*len(cumplidos)/(len(cumplidos)+len(pics))
+    rand = random.randint(0, 100)
+
+    if rand <= complimentPerc:
+        randomCommpliment = cumplidos[random.randint(
+            0, len(cumplidos)-1)]
+        try:
+            api.update_status(
+                "@{} {}".format(user, randomCommpliment))
+        except TweepError:
+            pass
+
+    elif rand > complimentPerc:
+        randomPic = pics[random.randint(
+            0, len(pics)-1)]
+        try:
+            api.update_with_media(filename=randomPic,
+                                  status="@{}".format(user))
+        except TweepError:
+            pass
+
+
+for mention in api.mentions_timeline()[:10]:
     mentionID = mention._json["id"]
     lastMentionsID2.append(mentionID)
 
 while True:
     lastMentionsID = []
-    lastMentions = api.mentions_timeline()[:5]
+    lastMentions = api.mentions_timeline()[:10]
     for mention in lastMentions:
         mentionedUsers = []
         userCont = 0
@@ -77,31 +96,20 @@ while True:
 
             for palabra in textMentionWords:
                 if palabra[0] == "@" and palabra != "@CumplidoBot":
-                    mentionedUsers.append(palabra)
+
+                    mentionedUsers.append(palabra.replace("@", ""))
                     userCont += 1
 
             if userCont == 0:
                 friendName = getRandomFriend(
                     mention._json["user"]["screen_name"])
-                randomCommpliment = cumplidos[random.randint(
-                    0, len(cumplidos)-1)]
 
-                try:
-                    api.update_status(
-                        "@{} {}".format(friendName, randomCommpliment))
-                except TweepError:
-                    pass
+                postCompliment(friendName)
 
             elif userCont > 0:
                 for user in mentionedUsers:
-                    randomCommpliment = cumplidos[random.randint(
-                        0, len(cumplidos)-1)]
+                    postCompliment(user)
 
-                    try:
-                        api.update_status("{} {}".format(
-                            user, randomCommpliment))
-                    except TweepError:
-                        pass
             time.sleep(5)
 
     lastMentionsID2 = lastMentionsID
---
+++ cumplidos.py
@@ -0,0 +1,13 @@
+#32
+cumplidos = ["Guaperas", "Hermosura", "❤❤❤❤❤", "😍😍😍😍😍😍😍😍😍", "🥵🥵🥵🥵🥵🥵🥵🥵🥵", "😳😳😳😳😳😳😳", "Bellesón", "No te pongas al sol, quel los bombones se derriten",
+             "Vaya, no sabía que había gente tan hermosa en Twitter", "Por ti, mi corazón palpita, como una patata frita", "La verdad que te quedan muy bien los sombreros", "Damn u really hot",
+             "🏆 Premio a la persona más guuapa de Twitter", "Eres como una rana: muy bonita", "Yo me haría pirata. No por el oro ni por la plata. Sino por ese tesoro, que tienes entre las patas",
+             "Vaya peinado más guay tienes", "Hola, somos de la Agencia Tributaria de Belleza. Venimos a cobrarle el impuesto de las grandes fortunas",
+             "Si fuese camionero, te llevaría en mi camión. Pero como no lo soy, te llevo en mi corazón",  "Que tengas un buen día :)",
+             "¡Buenas! Soy el bot medidor de las personas guapas. Según mis cálculos ¿usted tiene una belleza del 100%!", "Eres un sol", "Tu mirada me encandila",
+             "Quien fuera patatas fritas para acompañarte en tu lomo", "Eres más apañá que una braga de esparto"]
+
+#20
+pics = ["images/bakugo.jpg", "images/dolor_pecho.png", "images/elmo.jpg", "images/espiderman.jpg", "images/gatos.jpg", "images/gustavo1.png", "images/gustavo2.png", "images/gustavo3.jpg",
+        "images/gustavo4.jpg", "images/gustavo5.jpg", "images/kirby.jpg", "images/kiryu.jpeg", "images/office.png", "images/oso_gordo.jpg", "images/perro_gorra.jpeg", "images/perro_mensajero.jpeg",
+        "images/pez.png", "images/pollo_con_cuchillo.jpeg", "images/rebanada.jpg", "images/sailormoon.jpg"]

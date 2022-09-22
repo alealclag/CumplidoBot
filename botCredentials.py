3b72889c67349ed7ccb1fc03a4bc3af9c27144e2
alealclag <alealclag@gmail.com>
2022-09-22 21:09:43 +0200

    Changed credentials to env vars

--- botCredentials.py
+++ botCredentials.py
@@ -1,6 +1,8 @@
 import os
-API_KEY = os.getenv('API_KEY')
-API_SECRET_KEY = os.getenv('API_SECRET_KEY')
 
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-ACCESS_SECRET_TOKEN = os.getenv('ACCESS_SECRET_TOKEN')
+ACCESS_SECRET_TOKEN = os.environ.get("ACCESS_SECRET_TOKEN")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+API_KEY = os.environ.get("API_KEY")
+API_SECRET_KEY = os.environ.get("API_SECRET_KEY")
+
+
